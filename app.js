c3845466caa88c809a5efd08d0272e4cbb7527e0
Schitte Industries <schitteindustries@gmail.com>
2017-11-18 16:43:33 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var request = require('request');
+var app = express();
+
+app.use(bodyParser.urlencoded({
+ extended: true
+}));
+
+app.use(bodyParser.json());
+
+var teams = ["Coke", "Pepsi", "Beatles", "RollingStones", "PlayStation", "XBOX", "India", "Pakistan", "Marvel", "DC", "Ferrari", "Lamborghini"];
+
+app.get('/test', function (req, res) {
+ res.send('null');
+});
+
+app.get('/ping', function (req, res) {
+  //monitor pings here every hour.
+  for(var i=0; i < teams.length; i++) {
+    request({
+      url: "https://api.coinhive.com/user/balance",
+      method: "GET"
+    }, qs: {
+      name: teams[i],
+      secret: PROCESS.ENV.HIVE_SECRET;
+    },function(error, response, body) {
+      console.log("Response: ", response.balance);
+      console.log("Body: ", body.balance);
+    });
+  });
+  //fetch data from coinhive
+  //add data to firebase
+  
+}
+
+app.listen(process.env.PORT, function() {
+ console.log('Server running on ' + process.env.PORT);
+});
